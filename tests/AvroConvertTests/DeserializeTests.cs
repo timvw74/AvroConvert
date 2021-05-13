675d0865243236e80d79ac33a249a4c1dd652641
AdrianStrugala <strugala.adrian@gmail.com>
2021-05-13 11:50:50 +0200

    added issue38 test

--- tests/AvroConvertTests/DeserializeTests.cs
+++ tests/AvroConvertTests/DeserializeTests.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using SolTechnology.Avro;
 using SolTechnology.Avro.Exceptions;
 using Xunit;
@@ -7,21 +10,8 @@ namespace AvroConvertComponentTests
 {
     public class DeserializeTests
     {
-        private readonly byte[] _avroBytes = System.IO.File.ReadAllBytes("avro.avro");
+        private readonly byte[] _avroBytes = System.IO.File.ReadAllBytes("example2.avro");
 
-
-        [Fact]
-        public void Issue38()
-        {
-
-            string schema =
-                "{ \"type\":\"record\", \"name\":\"DepartmentAVRO\", \"namespace\":\"models.mvp.Department\", \"fields\":[ {\"name\":\"struct_code\",\"type\":\"int\",\"doc\":\"Идентификатор подразделения\"}, {\"name\":\"firm_id\",\"type\":\"int\",\"doc\":\"Идентификатор организации\"}, { \"name\":\"struct_parent\",\"type\":[\"int\",\"null\"],\"doc\":\"Идентификатор родительского подразделения\"}, { \"name\":\"struct_name\",\"type\":\"string\",\"doc\":\"Наименование подразделения\"}, { \"name\":\"financial_short_name\",\"type\":\"string\",\"doc\":\"Код ЦФО\"}, { \"name\":\"start_date\",\"type\":[\"null\", { \"type\": \"long\", \"logicalType\":\"date\"}],\"doc\":\"Дата начала действия\"}, { \"name\":\"end_date\",\"type\":[\"null\", { \"type\": \"long\", \"logicalType\":\"date\"}],\"doc\":\"Дата окончания действия\"} ]}";
-
-            //Act
-            var result = AvroConvert.DeserializeHeadless<DepartmentAVRO>(_avroBytes, schema);
-            //Assert
-            var x = 1;
-        }
 
         [Fact]
         public void Deserialize_CustomSchema_OnlyValuesFromCustomSchemaAreReturned()
---
+++ tests/AvroConvertTests/Issue38Test.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using SolTechnology.Avro;
+using SolTechnology.Avro.Exceptions;
+using Xunit;
+
+namespace AvroConvertComponentTests
+{
+    public class Issue38Tests
+    {
+        private readonly byte[] _avroBytes = System.IO.File.ReadAllBytes("avro.avro");
+
+
+        [Fact]
+        public void Issue38()
+        {
+
+            string schema =
+                "{ \"type\":\"record\", \"name\":\"DepartmentAVRO\", \"namespace\":\"models.mvp.Department\", \"fields\":[ {\"name\":\"struct_code\",\"type\":\"int\",\"doc\":\"Идентификатор подразделения\"}, {\"name\":\"firm_id\",\"type\":\"int\",\"doc\":\"Идентификатор организации\"}, { \"name\":\"struct_parent\",\"type\":[\"int\",\"null\"],\"doc\":\"Идентификатор родительского подразделения\"}, { \"name\":\"struct_name\",\"type\":\"string\",\"doc\":\"Наименование подразделения\"}, { \"name\":\"financial_short_name\",\"type\":\"string\",\"doc\":\"Код ЦФО\"}, { \"name\":\"start_date\",\"type\":[\"null\", { \"type\": \"long\", \"logicalType\":\"date\"}],\"doc\":\"Дата начала действия\"}, { \"name\":\"end_date\",\"type\":[\"null\", { \"type\": \"long\", \"logicalType\":\"date\"}],\"doc\":\"Дата окончания действия\"} ]}";
+
+            //Act
+            var bytesAfterChange = _avroBytes.Skip(5);
+
+            var result = AvroConvert.DeserializeHeadless<DepartmentAVRO>(bytesAfterChange.ToArray(), schema);
+
+
+            //Assert
+
+        }
+    }
+}
