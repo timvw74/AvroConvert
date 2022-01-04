1936520deac3c657acaac71964dab47ede977ed5
AdrianStrugala <strugala.adrian@gmail.com>
2022-01-04 13:37:20 +0100

    added size benchmark

---
+++ FielSizeBenchmark/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using AutoFixture;
+using GrandeBenchmark;
+using Newtonsoft.Json;
+using SolTechnology.Avro;
+
+namespace FielSizeBenchmark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+
+            var data100 = BuildData(1);
+            var data1k = BuildData(10);
+            var data10k = BuildData(100);
+            var data100k = BuildData(1000);
+            var data1m = BuildData(10000);
+
+
+            DoIT(data100);
+            DoIT(data1k);
+            DoIT(data10k);
+            DoIT(data100k);
+            DoIT(data1m);
+
+
+
+            Console.ReadLine();
+
+        }
+
+        private static void DoIT(IEnumerable<User> data100)
+        {
+            Console.WriteLine("---------------------------------------------------------");
+
+            var json100 = JsonConvert.SerializeObject(data100);
+            var serializedBytes100 = Encoding.UTF8.GetBytes(json100);
+
+            Console.WriteLine("Json " + serializedBytes100.Length);
+
+            Console.WriteLine("Json Gzip " + GzipJson(serializedBytes100).Length);
+
+            Console.WriteLine("Avro " + AvroConvert.Serialize(data100).Length);
+
+            Console.WriteLine("Avro Brotli " + AvroConvert.Serialize(data100, CodecType.Brotli).Length);
+        }
+
+
+        internal static User[] BuildData(int N)
+        {
+            var fixture = new AutoFixture.Fixture();
+
+            return fixture
+                .Build<User>()
+                .With(c => c.Offerings, fixture.CreateMany<Offering>(N).ToList())
+                .CreateMany(N)
+                .ToArray();
+        }
+
+        internal static byte[] GzipJson(byte[] uncompressedData)
+        {
+            using (var compressedStream = new MemoryStream())
+            using (var zipStream = new GZipStream(compressedStream, CompressionMode.Compress))
+            {
+                zipStream.Write(uncompressedData, 0, uncompressedData.Length);
+                zipStream.Close();
+                return compressedStream.ToArray();
+            }
+        }
+    }
+}
---
+++ tests/AvroConvertTests/Issue69.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using SolTechnology.Avro;
+using Xunit;
+
+namespace AvroConvertComponentTests
+{
+    public class Issue69
+    {
+        private readonly byte[] _avroBytes;
+        private readonly string _example2schema;
+
+        public Issue69()
+        {
+            _avroBytes = File.ReadAllBytes("bq-sample");
+
+            // _example2schema = File.ReadAllText("AvroSchema.json");
+        }
+
+        [Fact]
+        public void GetSchemaAsString_ValidBytes_SchemaIsReturned()
+        {
+            //Arrange
+
+            var schema = AvroConvert.GetSchema(_avroBytes);
+
+            var result = AvroConvert.Deserialize<List<Root>>(_avroBytes);
+
+            var schema2 = AvroConvert.GenerateSchema(typeof(MyModel));
+            
+            //Act
+            // var result = AvroConvert.DeserializeHeadless<MyModel>(_avroBytes, schema);
+            
+            //Assert
+            Assert.Equal(_example2schema, "schema2");
+        }
+
+    }
+
+
+    public class MyModel
+    {
+        public int Capacity { get; set; }
+        public int Count { get; set; }
+        public Root Root { get; set; }
+
+
+    }
+
+    public class Root
+    {
+        public string? tracker_id { get; set; }
+        public double? latitude { get; set; }
+        public double? longitude { get; set; }
+        public string? datetime { get; set; }
+        public double? altitude { get; set; }
+        public string? accelerometer { get; set; }
+        public double? speed { get; set; }
+    }
+}
